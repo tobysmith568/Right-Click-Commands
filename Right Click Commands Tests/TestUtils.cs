f4752b3e571ec2b037a63badb5908c9e93e18de5
Toby Smith <tobysmith568@hotmail.co.uk>
2019-04-11 23:40:22 +0100

    Implemented a reflective test helper class

---
+++ Right Click Commands Tests/TestUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Right_Click_Commands.Tests
+{
+    public static class TestUtils
+    {
+        /// <exception cref="FieldAccessException">Ignore.</exception>
+        /// <exception cref="TargetException">Ignore.</exception>
+        public static void SetPrivateField(this object subject, string field, object value)
+        {
+            subject.GetType()
+                .GetField(field, BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(subject, value);
+        }
+        /// <exception cref="FieldAccessException">Ignore.</exception>
+        /// <exception cref="TargetException">Ignore.</exception>
+        public static void SetPrivateAutoProperty(this object subject, string field, object value)
+        {
+            subject.GetType()
+                .GetField($"<{field}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)
+                .SetValue(subject, value);
+        }
+
+        /// <exception cref="FieldAccessException">Ignore.</exception>
+        /// <exception cref="TargetException">Ignore.</exception>
+        public static void SetPrivateStaticField(this object subject, string field, object value)
+        {
+            subject.GetType()
+                .GetField(field, BindingFlags.Static | BindingFlags.NonPublic)
+                .SetValue(subject, value);
+        }
+    }
+}
