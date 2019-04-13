96b7c36cde7be73ba2c4802819f6a36658f34412
Toby Smith <tobysmith568@hotmail.co.uk>
2019-04-13 18:52:00 +0100

    Added tests for ScriptAccessException

---
+++ Right Click Commands Tests/Models/Scripts/ScriptAccessExceptionTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Right_Click_Commands.Models.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Right_Click_Commands.Models.Scripts.Tests
+{
+    [TestFixture]
+    public class ScriptAccessExceptionTests
+    {
+        ScriptAccessException subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = null;
+        }
+
+        [Test]
+        public void Test_Constructor_ValidData()
+        {
+            string message = "This is an exception message";
+            Exception inner = new Exception("This is an inner exception");
+
+            subject = new ScriptAccessException(message, inner);
+
+            Assert.AreEqual(message, subject.Message);
+            Assert.AreSame(inner, subject.InnerException);
+        }
+    }
+}
