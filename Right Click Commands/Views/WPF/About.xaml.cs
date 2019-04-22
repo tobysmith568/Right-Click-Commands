6adc2d5b88afad8716f7135093b8eaa97b12e167
Toby Smith <tobysmith568@hotmail.co.uk>
2019-04-22 21:37:10 +0100

    Started to implement the about box

--- Right Click Commands/Views/WPF/About.xaml.cs
+++ Right Click Commands/Views/WPF/About.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Navigation;
 
 namespace Right_Click_Commands.Views.WPF
 {
@@ -19,12 +11,34 @@ namespace Right_Click_Commands.Views.WPF
     /// </summary>
     public partial class About : Window
     {
+        //  Constructors
+        //  ============
+
         public About()
         {
             InitializeComponent();
+
+            tbVersion.Text = "Version: v" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //  Events
+        //  ======
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Unable to open URL: {e.Uri.AbsoluteUri}", "Error opening link", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            e.Handled = true;
+        }
+
+        private void BtnCloSe_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
