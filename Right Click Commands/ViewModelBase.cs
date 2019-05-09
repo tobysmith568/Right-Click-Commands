ead1b3128b7ad50c211ea632d14f274dadb9c7b8
Toby Smith <tobysmith568@hotmail.co.uk>
2019-05-10 00:01:16 +0100

    Allowed properties to trigger updates on others

--- Right Click Commands/ViewModelBase.cs
+++ Right Click Commands/ViewModelBase.cs
@@ -17,12 +17,20 @@ namespace Right_Click_Commands
         //  Methods
         //  =======
 
-        protected void PropertyChanging<T>(T value, ref T variable, string propertyName)
+        protected void PropertyChanging<T>(T value, ref T variable, string propertyName, params string[] otherPropertyNames)
         {
             if (!EqualityComparer<T>.Default.Equals(value, variable))
             {
                 variable = value;
                 RaisePropertyChanged(propertyName);
+
+                if (otherPropertyNames != null)
+                {
+                    foreach (string property in otherPropertyNames)
+                    {
+                        RaisePropertyChanged(property);
+                    }
+                }
             }
         }
 
