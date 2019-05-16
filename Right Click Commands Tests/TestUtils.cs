990517e20be0beabf1e90dae19a4fb767c8dc9a8
Toby Smith <tobysmith568@hotmail.co.uk>
2019-05-16 21:51:31 +0100

    Removed unused methods

--- Right Click Commands Tests/TestUtils.cs
+++ Right Click Commands Tests/TestUtils.cs
@@ -17,22 +17,5 @@ namespace Right_Click_Commands.Tests
                 .GetField(field, BindingFlags.Instance | BindingFlags.NonPublic)
                 .SetValue(subject, value);
         }
-        /// <exception cref="FieldAccessException">Ignore.</exception>
-        /// <exception cref="TargetException">Ignore.</exception>
-        public static void SetPrivateAutoProperty(this object subject, string field, object value)
-        {
-            subject.GetType()
-                .GetField($"<{field}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)
-                .SetValue(subject, value);
-        }
-
-        /// <exception cref="FieldAccessException">Ignore.</exception>
-        /// <exception cref="TargetException">Ignore.</exception>
-        public static void SetPrivateStaticField(this object subject, string field, object value)
-        {
-            subject.GetType()
-                .GetField(field, BindingFlags.Static | BindingFlags.NonPublic)
-                .SetValue(subject, value);
-        }
     }
 }
