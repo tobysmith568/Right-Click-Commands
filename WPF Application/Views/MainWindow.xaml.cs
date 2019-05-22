a299e6eec213a6de2c8e181266b9caee911d6bf5
Toby Smith <tobysmith568@hotmail.co.uk>
2019-05-22 23:56:36 +0100

    Refactored out the focusStealer

--- WPF Application/Views/MainWindow.xaml.cs
+++ WPF Application/Views/MainWindow.xaml.cs
@@ -46,7 +46,6 @@ namespace Right_Click_Commands.WPF.Views
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            FocusStealer.Focus();
             VMClosingEvent.Command.Execute(e);
         }
 
