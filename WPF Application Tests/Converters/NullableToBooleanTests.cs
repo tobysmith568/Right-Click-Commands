370545bc263de1e142cc7fb1cbef6740397441fe
Toby Smith <tobysmith568@hotmail.co.uk>
2019-06-02 21:43:46 +0100

    Disabled certain menu opens when there's no selected script

---
+++ WPF Application Tests/Converters/NullableToBooleanTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using System;
+using System.Windows;
+
+namespace Right_Click_Commands.WPF.Converters.Tests
+{
+    [TestFixture]
+    public class NullableToBooleanTests
+    {
+        NullableToBoolean subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = new NullableToBoolean();
+        }
+
+        #region Convert
+
+        [Test]
+        public void Convert_Null()
+        {
+            object result = subject.Convert(null, null, null, null);
+
+            Assert.IsInstanceOf(typeof(bool), result);
+
+            bool visibility = (bool)result;
+
+            Assert.IsFalse(visibility);
+        }
+
+        [Test]
+        public void Convert_Object()
+        {
+            object result = subject.Convert(new object(), null, null, null);
+
+            Assert.IsInstanceOf(typeof(bool), result);
+
+            bool visibility = (bool)result;
+
+            Assert.IsTrue(visibility);
+        }
+
+        [Test]
+        public void Convert_int()
+        {
+            object result = subject.Convert(4, null, null, null);
+
+            Assert.IsInstanceOf(typeof(bool), result);
+
+            bool visibility = (bool)result;
+
+            Assert.IsTrue(visibility);
+        }
+
+        #endregion
+        #region ConvertBack
+
+        [Test]
+        public void ConvertBack_True()
+        {
+            object result = subject.ConvertBack(true, null, null, null);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void ConvertBack_False()
+        {
+            object result = subject.ConvertBack(false, null, null, null);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void ConvertBack_Null()
+        {
+            Exception e = Assert.Throws<ArgumentException>(() => subject.ConvertBack(null, null, null, null));
+            Assert.AreEqual("The given value must be a boolean", e.Message);
+        }
+
+        [Test]
+        public void ConvertBack_UnepectedObject()
+        {
+            Exception e = Assert.Throws<ArgumentException>(() => subject.ConvertBack(new Style(), null, null, null));
+            Assert.AreEqual("The given value must be a boolean", e.Message);
+        }
+
+        #endregion
+    }
+}
---
+++ WPF Application/Converters/NullableToBoolean.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Right_Click_Commands.WPF.Converters
+{
+    public class NullableToBoolean : ConverterBase<NullableToBoolean>
+    {
+        //  Methods
+        //  =======
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                throw new ArgumentException("The given value must be a boolean");
+
+            return ((bool)value) ? new object() : null;
+        }
+    }
+}
